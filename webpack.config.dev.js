9eb1667814ddca1b6657802d014469f2e2fccbca
Carlos Queiroz <carlostgti@outlook.com>
2021-01-23 20:29:20 -0300

    adding mocking api and reducers

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -25,6 +25,9 @@ module.exports = {
     https: false,
   },
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env.API_URL": JSON.stringify("http://localhost:3001"),
+    }),
     new HtmlWebpackPlugin({
       template: "src/indexx.html",
       favicon: "src/favicon.ico",
