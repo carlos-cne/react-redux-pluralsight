cc15dc81642348301ae09539db14894140e99cb4
Carlos Queiroz <carlostgti@outlook.com>
2021-01-24 16:22:35 -0300

    adding jest and some test cases

---
+++ src/components/common/Header.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { mount, shallow } from "enzyme";
+import { MemoryRouter } from "react-router-dom";
+import Header from "./Header";
+
+it("contains 3 NavLinks via shallow", () => {
+  const numLinks = shallow(<Header />).find("NavLink").length;
+  expect(numLinks).toEqual(3);
+});
+
+it("contains 3 anchors via mount", () => {
+  const numAnchors = mount(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  ).find("a").length;
+
+  expect(numAnchors).toEqual(3);
+});
